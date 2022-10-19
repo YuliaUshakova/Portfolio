3d6ee8e94e5d2696173f8ac03ff1e540dc11f8e5
Yulia Ushakova <yulia.ushakova@.com>
2022-10-19 14:59:34 +0100

    upd project Eng Vint Home: title and link to

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.to(".hello", {
+    text: "Hello, World. I am Yulia Ushakova, a front-end developer from London",
+    delay: 1,
+    duration: 3,
+    ease: "power1.in"})
+
+    gsap.from(".imgLondon", {x: 700, delay: 4, duration: 2, ease: "power2.out"})
+
+    // gsap.from("nav", {y: -200, delay: 5, duration: 1, ease: "power2.out"})
+
+  
+        gsap.to(".profilePic", {
+        opacity: 1, delay: 8, duration: 2
+    })
+    
