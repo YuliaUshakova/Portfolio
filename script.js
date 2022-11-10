684610a630c2e9ab32863bf20cde2cafc7bd8c38
Juls-1979 <yulia.ushakova@gmail.com>
2022-11-10 11:24:45 +0000

    css adjusted

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ gsap.to(".hello", {
     duration: 3,
     ease: "power1.in"})
 
-    gsap.from(".imgLondon", {x: 700, delay: 4, duration: 2, ease: "power2.out"})
+    gsap.from(".imgLondon", {y: -700, delay: 4, duration: 2, ease: "power2.out"})
 
-    // gsap.from("nav", {y: -200, delay: 5, duration: 1, ease: "power2.out"})
-
-  
         gsap.to(".profilePic", {
         opacity: 1, delay: 8, duration: 2
     })
